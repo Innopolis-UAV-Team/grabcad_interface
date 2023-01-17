9b11b7eb3ae737e73dc062419cd0b02dbd024f02
Jlo6CTEP <19yura99@mail.ru>
2023-01-17 18:04:34 +0300

    dockerize the app

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while getopts e:p:u: flag
+do
+    case "${flag}" in
+        e) email=${OPTARG};;
+        p) password=${OPTARG};;
+        u) url=${OPTARG};;
+        *)
+    esac
+done
+
+mkdir ./vtol
+cd ./vtol || return
+
+python3 /grabcad/pygc.py init --email "${email}" --pass "${password}" --url "${url}" --dont_save_creds true
+python3 /grabcad/pygc.py pull --email "${email}" --pass "${password}" --dont_save_creds true
